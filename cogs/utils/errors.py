944b2616286339cd15808996536354bea07fc12f
HARU <948guppy@gmail.com>
2020-08-23 18:04:28 +0900

    [ADD]Adminの追加とutilsの追加

---
+++ cogs/utils/errors.py
@@ -0,0 +1,19 @@
+class PermissionNotFound(Exception):
+    pass
+
+
+class NotGuildChannel(Exception):
+    pass
+
+
+class NotDMChannel(Exception):
+    pass
+
+
+
+class IncompletePreparing(Exception):
+    pass
+
+
+class InvalidArgument(Exception):
+    pass
---
+++ cogs/utils/table.py
@@ -0,0 +1,5 @@
+class Leagues():
+    pass
+
+class Tournaments():
+    pass
